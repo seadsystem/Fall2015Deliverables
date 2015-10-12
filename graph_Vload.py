9d11dcdc0194bb91a0ed0a838b5f94f42aebd8e4
SydBal <dbalasso@ucsc.edu>
2015-10-12 15:37:22 -0700

    Update graph_Vload.py
    This version only grabs the V(oltage) readings

--- graph_Vload.py
+++ graph_Vload.py
@@ -10,14 +10,23 @@
 # a python matrix
 #
 # TODO:
-# Only add voltage (V) readings to the matrix
+# Allow user to designate file to input
 # Graph these (unnecessary for API)
 
 import numpy as np
+import csv
 
-my_matrix = []
-for line in open('split_rawax.csv'):
-    temp_matrix = line.split()
-    my_matrix.append(temp_matrix)
+#v_matrix will hold all the V(oltage) values from the file
+v_matrix = []
 
-my_matrix
+#temp_reader will hold data directly from file in csv object
+#QUOTE_NONE ignores quotes, for simplicity
+temp_reader = csv.reader(open('split_rawax.csv','rb'), quoting=csv.QUOTE_NONE)
+
+for row in temp_reader:
+    # check for Volatage tag (V)
+    row_data = next(temp_reader)
+    if row_data[1] == 'V':
+        v_matrix.append(row_data)
+
+print(v_matrix)
